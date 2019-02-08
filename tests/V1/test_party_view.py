dc17877f5438cf675b2b10aa24479448ca1579c8
webmaster254 <josephm2800@gmail.com>
2019-02-08 15:35:35 +0300

    [feature #163722750]reated tests for party model and view

---
+++ tests/V1/test_party_view.py
@@ -0,0 +1,154 @@
+import unittest
+import json
+from app import create_app
+
+
+class TestPartiesRoute(unittest.TestCase):
+    """ Test case for party views """
+
+    def setUp(self):
+        """ Init app and test vars """
+        self.app = create_app(config_name="testing")
+        self.client = self.app.test_client
+        self.party_reg_data = {
+            "name": "Jubilee",
+            "hqAddress": "Jubilee Tower, Pangani, Thika Road",
+            "logoUrl": "/static/jubilee.jpeg",
+            "Party members": 225
+        }
+
+    def test_party_creation_with_valid_data(self):
+        """test with valid data - 201 (created) + data"""
+        response = self.client().post(
+            "/api/v1/parties",
+            data=json.dumps(self.party_reg_data),
+            headers={'content-type': 'application/json'}
+        )
+        self.assertEqual(
+            response.status_code, 201,
+            msg="response code SHOULD BE 201 (Created)"
+        )
+
+    def test_with_more_fields_than_expected(self):
+        """ Test with more fields than expected. """
+        test_reg_data = {
+            "name": "Jubilee",
+            "hqAddress": "Jubilee Tower, Pangani, Thika Road",
+            "logoUrl": "/static/jubilee.jpeg",
+            "Party members": 225,
+            "nickname": "I'm not supposed to be here"
+        }
+
+        response = self.client().post(
+            "/api/v1/parties",
+            data=json.dumps(test_reg_data),
+            headers={'content-type': 'application/json'}
+        )
+        deserialized_response = json.loads(response.data.decode())
+        self.assertEqual(
+            response.status_code, 400,
+            msg="response code SHOULD BE 400 (bad query) + msg"
+        )
+        self.assertEqual(
+            deserialized_response["error"], "More data fields than expected",
+            msg="Response Body Contents- Should be custom message "
+        )
+
+    def test_with_fewer_fields_than_expected(self):
+        """ Test with fewer fields than expected """
+        test_reg_data = {
+            "name": "Jubilee",
+            "hqAddress": "Jubilee Tower, Pangani, Thika Road",
+            "logoUrl": "/static/jubilee.jpeg"
+        }
+
+        response = self.client().post(
+            "/api/v1/parties",
+            data=json.dumps(test_reg_data),
+            headers={'content-type': 'application/json'}
+        )
+        deserialized_response = json.loads(response.data.decode())
+        self.assertEqual(
+            response.status_code, 400,
+            msg="response code SHOULD BE 400 (bad query) + msg"
+        )
+        self.assertEqual(
+            deserialized_response["error"], "Fewer data fields than expected",
+            msg="Response Body Contents- Should be custom message "
+        )
+
+    def test_with_an_empty_string_in_field(self):
+        """ Test with an empty string in field """
+        test_reg_data = {
+            "name": "",
+            "hqAddress": "Jubilee Tower, Pangani, Thika Road",
+            "logoUrl": "/static/jubilee.jpeg",
+            "Party members": 225,
+        }
+
+        response = self.client().post(
+            "/api/v1/parties",
+            data=json.dumps(test_reg_data),
+            headers={'content-type': 'application/json'}
+        )
+        deserialized_response = json.loads(response.data.decode())
+        self.assertEqual(
+            response.status_code, 422,
+            msg="response code SHOULD BE 422 (Unprocessable Entity) + msg"
+        )
+        self.assertEqual(
+            deserialized_response["error"], "Empty data field",
+            msg="Response Body Contents- Should be custom message "
+        )
+
+    def test_with_invalid_value_types(self):
+        """test with invalid value types  - 422 (Unprocessable Entity) + msg
+        """
+        test_reg_data = {
+            "name": 12,
+            "hqAddress": "Jubilee Tower, Pangani, Thika Road",
+            "logoUrl": "/static/jubilee.jpeg",
+            "Party members": 225
+        }
+
+        response = self.client().post(
+            "/api/v1/parties",
+            data=json.dumps(test_reg_data),
+            headers={'content-type': 'application/json'}
+        )
+        deserialized_response = json.loads(response.data.decode())
+        self.assertEqual(
+            response.status_code, 422,
+            msg="response code SHOULD BE 422 (Unprocessable Entity) + msg"
+        )
+        self.assertEqual(
+            deserialized_response["error"], "Invalid value in data field",
+            msg="Response Body Contents- Should be custom message "
+        )
+
+    def test_with_already_created_party(self):
+        """ Test that a party cannot be created twice
+        """
+        response = self.client().post(
+            "/api/v1/parties",
+            data=json.dumps(self.party_reg_data),
+            headers={'content-type': 'application/json'}
+        )
+        deserialized_response = json.loads(response.data.decode())
+        self.assertEqual(
+            response.status_code, 201,
+            msg="response code SHOULD BE 201 (Created)"
+        )
+        response = self.client().post(
+            "/api/v1/parties",
+            data=json.dumps(self.party_reg_data),
+            headers={'content-type': 'application/json'}
+        )
+        deserialized_response = json.loads(response.data.decode())
+        self.assertEqual(
+            deserialized_response["error"], "Party already exists",
+            msg="Response Body Contents- Should be custom message "
+        )
+
+if __name__ == "__main__":
+    unittest.main()
